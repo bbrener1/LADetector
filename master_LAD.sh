c65b902018337ebf7ba691e56f2596fed9223320
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-10-12 19:36:19 -0400

    daily

--- master_LAD.sh
+++ master_LAD.sh
@@ -10,8 +10,9 @@ module load anaconda-python
 module load samtools
 # source activate reddy_cancer
 
-./scripts/fusor.sh $(find $(readlink -f $1) -iname *.gz) > $2
 echo "$1"
-echo "$(find $(readlink -f $1) --iname *.gz)"
+echo "$(find $(readlink -f $1) -iname *.gz)"
 echo "$2"
+./scripts/fusor.sh $(find $(readlink -f $1) -iname *.gz) > $2
+
 # sbatch ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz --slurm 1
