1f7e489f2ab835e7141facfd94fd2fdfa407e4f3
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-10-12 14:55:30 -0400

    bugfixes

--- master_LAD.sh
+++ master_LAD.sh
@@ -1,1 +1,1 @@
-srun ./scripts/pre-normalization-scoring.sh --input $1 --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz
+srun ./scripts/pre-normalization-scoring.sh --input $(readlink -f $1) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz
