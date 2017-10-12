f82d877594b15d43e0725c3db600735575356faf
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-10-12 17:09:16 -0400

    fusor fix

--- master_LAD.sh
+++ master_LAD.sh
@@ -5,10 +5,12 @@
 #SBATCH --partition=parallel
 #SBATCH -t 500
 
+module unload git
 module load anaconda-python
 module load samtools
 source activate reddy_cancer
 
 srun ./scripts/fusor.sh $1 > $2
-
-sbatch ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz --slurm 1
+echo "$1"
+echo "$2"
+# sbatch ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz --slurm 1
