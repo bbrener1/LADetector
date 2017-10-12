a0c91a7f25ed117bc7ebfced0d7ffcbdb72ebc29
Boris Brenerman <bbrener1@jhu.edu@login-node02.cm.cluster>
2017-10-12 19:33:53 -0400

    oops

--- master_LAD.sh
+++ master_LAD.sh
@@ -10,8 +10,8 @@ module load anaconda-python
 module load samtools
 # source activate reddy_cancer
 
-srun ./scripts/fusor.sh $(find $(readlink -f $1) -iname *.gz) > $2
+./scripts/fusor.sh $(find $(readlink -f $1) -iname *.gz) > $2
 echo "$1"
-echo "$(ls $1 | grep .gz)"
+echo "$(find $(readlink -f $1) --iname *.gz)"
 echo "$2"
 # sbatch ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz --slurm 1
