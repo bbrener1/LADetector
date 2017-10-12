18005a484984c8cd3fd11d808b7f9450a464029a
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2017-10-12 17:33:18 -0400

    daily

--- master_LAD.sh
+++ master_LAD.sh
@@ -10,7 +10,7 @@ module load anaconda-python
 module load samtools
 source activate reddy_cancer
 
-srun ./scripts/fusor.sh $1 > $2
+srun ./scripts/fusor.sh $1/*.gz > $2
 echo "$1"
 echo "$2"
 # sbatch ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2) --scripts ./scripts/ --build ./data --bins ./data/DpnIIbins_hg38.bed.gz --slurm 1
