28667f85ed3896f3dfc21bdad238b495dd14c11d
Boris M Brenerman <boris@tvmac.hwcampus.jhu.edu>
2017-10-15 17:05:07 -0400

    daily

--- scripts/fusor.sh
+++ scripts/fusor.sh
@@ -3,10 +3,12 @@
 echo "Starting fusion"
 echo $0
 echo "" > $1
+target=$1
 shift
 while [[ "$#" > 0 ]]; do
     echo "Fusing $1"
-    zcat $1 >> $1;
+    zcat $1 >> $target;
+    ls -lh $target
     shift;
 done
 echo "Finished fusion"
