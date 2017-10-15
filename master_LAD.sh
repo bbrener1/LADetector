309264a14f5151b1e9f10d2f94be0c4b7eb1043f
Boris M Brenerman <boris@tvmac.hwcampus.jhu.edu>
2017-10-15 17:24:30 -0400

    daily

--- master_LAD.sh
+++ master_LAD.sh
@@ -17,21 +17,16 @@ echo "$2"
 
 for p in $(cat $1);
 do
-  echo "We are in the loop"
-  echo $(cat $1)
-  echo $p
+  echo "Processing $p"
   i=$(basename $p)
-  echo "$i"
   ./scripts/fusor.sh $2$i.dam.fused.fastq $(find $(readlink -f $p/Dam/) -iname *.gz);
 
-  echo "I finished fusing, now I'm in master_LAD body!"
+  ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.dam.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 40
 
-  # ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.dam.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 40
-  #
-  # ./scripts/fusor.sh $(find $(readlink -f $i/LmnB/) -iname *.gz) > $2$i.lmnb.fused.fastq;
-  #
-  # ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.lmnB.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 40
+  ./scripts/fusor.sh $(find $(readlink -f $i/LmnB/) -iname *.gz) > $2$i.lmnb.fused.fastq;
 
-  # perl $local_absolute/scripts/Normalization.pl $2$i.dam.fused.preNormalization.score $2$i.lmnB.fused.preNormalization.score $2$i.dam.fused.mappedReadCounts $2$i.lmnb.fused.mappedReadCounts $2$i.normalized
+  ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.lmnB.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 40
+
+  perl $local_absolute/scripts/Normalization.pl $2$i.dam.fused.preNormalization.score $2$i.lmnB.fused.preNormalization.score $2$i.dam.fused.mappedReadCounts $2$i.lmnb.fused.mappedReadCounts $2$i.normalized
 
 done
--- scripts/fusor.sh
+++ scripts/fusor.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Starting fusion"
-for i in $(seq 1 $#);
-do
-  echo $i
-done
 echo "" > $1
 target=$1
 shift
 while [[ "$#" > 0 ]]; do
     echo "Fusing $1"
     zcat $1 >> $target;
-    ls -lh $target
     shift;
 done
 echo "Finished fusion"
