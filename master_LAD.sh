5abdeef345cb07a83d8b0f507352c259a92c5c68
Boris M Brenerman <boris@biol-cmdb-24.hwcampus.jhu.edu>
2017-10-16 01:36:31 -0400

    daily

--- master_LAD.sh
+++ master_LAD.sh
@@ -20,14 +20,14 @@ do
   echo "Processing $p"
   i=$(basename $p)
 
-  # echo "Fusing dam"
-  # ./scripts/fusor.sh $(find $(readlink -f $p/Dam/) -iname *.gz) > $2$i.dam.fused.fastq;
-  #
-  # echo "Fusing done"
-  #
-  # ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.dam.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 22
-  #
-  # echo "Dam scoring complete"
+  echo "Fusing dam"
+  ./scripts/fusor.sh $(find $(readlink -f $p/Dam/) -iname *.gz) > $2$i.dam.fused.fastq;
+
+  echo "Fusing done"
+
+  ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.dam.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 22
+
+  echo "Dam scoring complete"
 
   echo "Fusing lmnb"
   ./scripts/fusor.sh $(find $(readlink -f $p/LmnB/) -iname *.gz) > $2$i.lmnb.fused.fastq;
@@ -35,7 +35,7 @@ do
   echo "Fusing done"
 
   ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.lmnb.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 22
-  #
-  # perl $local_absolute/scripts/Normalization.pl $2$i.dam.fused.preNormalization.score $2$i.lmnB.fused.preNormalization.score $2$i.dam.fused.mappedReadCounts $2$i.lmnb.fused.mappedReadCounts $2$i.normalized
+
+  perl $local_absolute/scripts/Normalization.pl $2$i.dam.fused.preNormalization.score $2$i.lmnB.fused.preNormalization.score $2$i.dam.fused.mappedReadCounts $2$i.lmnb.fused.mappedReadCounts $2$i.normalized
 
 done
