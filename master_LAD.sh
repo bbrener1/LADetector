064340081009282334170d9d2fe75e59b4e56bd4
Boris M Brenerman <boris@biol-cmdb-24.hwcampus.jhu.edu>
2017-10-16 00:28:07 -0400

    daily

--- master_LAD.sh
+++ master_LAD.sh
@@ -29,12 +29,12 @@ do
   #
   # echo "Dam scoring complete"
 
-  echo "Fusing lmnb"
-  ./scripts/fusor.sh $(find $(readlink -f $p/LmnB/) -iname *.gz) > $2$i.lmnb.fused.fastq;
+  # echo "Fusing lmnb"
+  # ./scripts/fusor.sh $(find $(readlink -f $p/LmnB/) -iname *.gz) > $2$i.lmnb.fused.fastq;
+  #
+  # echo "Fusing done"
 
-  echo "Fusing done"
-
-  # ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.lmnB.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 22
+  ./scripts/pre-normalization-scoring.sh --input $(readlink -f $2$i.lmnB.fused.fastq) --scripts $local_absolute/scripts/ --build $local_absolute/data/GCA_000001405.15_GRCh38_no_alt_analysis_set --bins $local_absolute/data/DpnIIbins_hg38.bed --slurm 22
   #
   # perl $local_absolute/scripts/Normalization.pl $2$i.dam.fused.preNormalization.score $2$i.lmnB.fused.preNormalization.score $2$i.dam.fused.mappedReadCounts $2$i.lmnb.fused.mappedReadCounts $2$i.normalized
 
